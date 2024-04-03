c5de73ace85268cd72427d38bf8b40adcb8ab6af
James Boyle <james.boyle@utexas.edu>
2024-04-03 00:18:47 -0500

    sim.py: Support conv and connected layers
    Add a new tutorial script which will generate the DVS gesture
    benchmark from a given Keras (.h5) file. This mimics hacky code
    added into the SNN toolbox. Add support in SANA-FE for defining
    convolutional and fully connected layers.
    
    [wip] Still needs to be cleaned up a bit, and to figure out how
    users will define the mapping. Also add a function to check the
    network mapping is valid i.e.,
    
    * all neurons are mapped to one core
    * no core has more than 1024 neurons mapped
    * no core has more than 4096 axons going out, assuming there is no
      sharing

--- scripts/tutorial.py
+++ scripts/tutorial.py
@@ -8,67 +8,47 @@ tutorial.py: NICE 2024 Tutorial
 """
 import sys
 import os
-#from tensorflow.keras import models
+from tensorflow.keras import models
 import numpy as np
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.abspath((os.path.join(SCRIPT_DIR, os.pardir)))
-NETWORK_PATH = os.path.join(PROJECT_DIR, "snn", "dvs_challenge.net")
-ARCH_PATH = os.path.join(PROJECT_DIR, "arch", "loihi.yaml")
-TIMESTEPS = 1000
-
 sys.path.insert(0, PROJECT_DIR)
 import sim
 
 def check_mapping(network):
-    # Check that all neurons are mapped to a core
-    neurons_mapped_to_cores = [0 for _ in range(0, 128)]
-    for n in network.neurons:
-        if n.core is None:
-            print("Error: Neuron n{n.id} is not mapped!")
-            return False
-        else:
-            neurons_mapped_to_cores[n.core] += 1
-
-    for core in range(0, 128):
-        if neurons_mapped_to_cores[core] > 1024:
-            print("Error: Core {core} has > 1024 neurons mapped")
-            return False
-
     return True
 
+# Load the Keras model for the DVS gesture categorization benchmark
+model = models.load_model(os.path.join(PROJECT_DIR, "etc", "dvs_challenge.h5"))
 network = sim.Network()
-arch = sim.Architecture()
+compartments = sim.init_compartments(32, 128, 1024)
 
-snn = np.load(os.path.join(PROJECT_DIR, "..", "dvs_challenge.npz"))
-# TODO: save the updated model so we just have to load the model and the weights
 # Convert the DVS gesture categorization model to SANA-FE's format
+print(model.summary())
+layer0 = sim.create_layer(network, 1024, compartments)
 
-biases = snn["inputs"]
-thresholds = snn["thresholds"]
-layer0 = sim.create_layer(network, 1024, threshold=thresholds[0], biases=biases)
-layer1 = sim.create_conv_layer(network, layer0, (32, 32, 1),  # 3600 neurons
-                               snn["conv1"], stride=2, threshold=thresholds[1],
-                               )
-layer2 = sim.create_conv_layer(network, layer1, (15, 15, 16),  # 5408 neurons
-                               snn["conv2"], stride=1, threshold=thresholds[2])
-layer3 = sim.create_conv_layer(network, layer2, (13, 13, 32),  # 7744 neurons
-                               snn["conv3"], stride=1, threshold=thresholds[3])
-layer4 = sim.create_conv_layer(network, layer3, (11, 11, 64),  # 891 neurons
-                               snn["conv4"], stride=1, threshold=thresholds[4])
-layer5 = sim.create_connected_layer(network, layer4, (9, 9, 11),  # 11 neurons
-                                    snn["dense1"], threshold=thresholds[5])
+filters = model.layers[0].get_weights()[0]
+layer1 = sim.create_conv_layer(network, layer0, (32, 32, 1),
+                               filters, compartments, stride=2)
+filters = model.layers[1].get_weights()[0]
+layer2 = sim.create_conv_layer(network, layer1, (15, 15, 16),
+                               filters, compartments, stride=1)
+filters = model.layers[2].get_weights()[0]
+layer3 = sim.create_conv_layer(network, layer2, (13, 13, 32),
+                               filters, compartments, stride=1)
+filters = model.layers[3].get_weights()[0]
+layer4 = sim.create_conv_layer(network, layer3, (11, 11, 64),
+                               filters, compartments, stride=1)
 
-sim.map_neuron_group_to_cores(layer0, arch, 1)
-sim.map_neuron_group_to_cores(layer1, arch, 4)
-sim.map_neuron_group_to_cores(layer2, arch, 16)
-sim.map_neuron_group_to_cores(layer3, arch, 16)
-sim.map_neuron_group_to_cores(layer4, arch, 4)
-sim.map_neuron_group_to_cores(layer5, arch, 1)
+weights = model.layers[5].get_weights()[0]
+# Rearrange the Keras weights to be channel second
+weights = np.reshape(weights, (9, 9, 11, 11))
+weights = np.swapaxes(weights, 2, 1)
+weights = np.reshape(weights, (891, 11), order='C')
+layer5 = sim.create_connected_layer(network, layer4, (9, 9, 11), weights,
+                                    compartments)
 
 # Save the network to file. Note that this file may require a large amount of
 #  disk space
-network.save(NETWORK_PATH, save_mappings=True)
-# Run the network you just generated on Loihi
-# Comment out this line to stop simulation runs:w
-sim.run(ARCH_PATH, NETWORK_PATH, TIMESTEPS)
+network.save(os.path.join("dvstmp.net"))
--- sim.py
+++ sim.py
@@ -268,56 +268,18 @@ def map_neuron_to_compartment(arch, core=None):
     return None, None
 
 
-def map_neuron_group_to_cores(neuron_group, arch, core_count):
-    return map_neurons_to_cores(neuron_group.neurons, arch, core_count)
-
-
-def map_neurons_to_cores(neurons, arch, core_count):
-    """Map neurons to one or more cores"""
-    neuron_count = len(neurons)
-    neurons_per_core = neuron_count // core_count
-    print(f"Mapping {neuron_count} neurons to {core_count} cores")
-
-    # Map N sequential cores. Find the first empty core
-    first_unused_core = arch.compartments.index(arch.max_compartments)
-
-    # Create a list of the cores to map to (absolute ID)
-    cores = list(range(first_unused_core, (first_unused_core+core_count)))
-
-    # Go through list and assign compartments of each entry
-    map_neurons = [neurons_per_core] * (core_count)
-    map_neurons[-1] += (neuron_count - (neurons_per_core*core_count))
-    assert(sum(map_neurons) == neuron_count)
-
-    mapping = dict(zip(cores, map_neurons))
-    #print(f"Mapping dictionary: {mapping}")
-
-    curr_neuron = 0
-    for core in mapping.keys():
-        # For the last core, map all remaining neurons
-        for _ in range(0, mapping[core]):
-            n = neurons[curr_neuron]
-            mapped_core = map_neuron_to_compartment(arch,
-                                                    core=core)
-            n.tile, n.core = arch.core_to_address[mapped_core]
-            curr_neuron += 1
-
-    assert(curr_neuron == neuron_count)
-    return mapping
-
-
 def create_connected_layer(network, prev_layer, input_shape, weights,
-                           log_spikes=False, log_potential=False,
+                           compartments, log_spikes=False, log_potential=False,
                            force_update=False, threshold=1.0, reset=0.0,
-                           leak=1.0, reverse_threshold=None,
+                           leak=1.0, mappings=None, reverse_threshold=None,
                            reverse_reset_mode=None, soma_hw_name=None,
                            synapse_hw_name=None):
     layer_neuron_count = weights.shape[1]
-    layer = create_layer(network, layer_neuron_count,
+    layer = create_layer(network, layer_neuron_count, compartments,
                          log_spikes=log_spikes, log_potential=log_potential,
                          force_update=force_update,
                          threshold=threshold, reset=reset, leak=leak,
-                         connections_out=None,
+                         mappings=mappings, connections_out=None,
                          reverse_threshold=reverse_threshold,
                          reverse_reset_mode=reverse_reset_mode,
                          soma_hw_name=soma_hw_name,
@@ -332,10 +294,10 @@ def create_connected_layer(network, prev_layer, input_shape, weights,
     return layer
 
 
-def create_conv_layer(network, input_layer, input_shape, filters,
+def create_conv_layer(network, input_layer, input_shape, filters, compartments,
                       stride=1, pad=0, log_spikes=False, log_potential=False,
                       force_update=False, threshold=1.0, reset=0.0,
-                      leak=1.0, reverse_threshold=None,
+                      leak=1.0, mappings=None, reverse_threshold=None,
                       reverse_reset_mode=None, soma_hw_name=None,
                       synapse_hw_name=None):
 
@@ -356,12 +318,12 @@ def create_conv_layer(network, input_layer, input_shape, filters,
     assert(kernel_channels == input_channels)
 
     layer_neuron_count = output_channels * output_width * output_height
-    output_layer = create_layer(network, layer_neuron_count,
+    output_layer = create_layer(network, layer_neuron_count, compartments,
                                 log_spikes=log_spikes,
                                 log_potential=log_potential,
                                 force_update=force_update,
                                 threshold=threshold, reset=reset, leak=leak,
-                                connections_out=None,
+                                mappings=mappings, connections_out=None,
                                 reverse_threshold=reverse_threshold,
                                 reverse_reset_mode=reverse_reset_mode,
                                 soma_hw_name=soma_hw_name,
@@ -396,7 +358,7 @@ def create_conv_layer(network, input_layer, input_shape, filters,
     return output_layer
 
 
-def create_layer(network, layer_neuron_count,
+def create_layer(network, layer_neuron_count, compartments,
                  log_spikes=False, log_potential=False, force_update=False,
                  threshold=1.0, reset=0.0, leak=1.0,
                  connections_out=None, reverse_threshold=None,
