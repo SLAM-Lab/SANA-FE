cd69e2b669bb00e3ab5a8237eb29b3c22ef45aa0
James Boyle <james.boyle@utexas.edu>
2024-04-08 19:49:16 -0500

    Attempt 3 at fixing docker

--- setup_tutorial.sh
+++ setup_tutorial.sh
@@ -1,7 +1,7 @@
-mkdir /tutorial/files
-mv /tutorial/example.net tutorial/files
-mv /tutorial/tutorial.net tutorial/files
-mv /tutorial/loihi.yaml tutorial/files
-mv /tutorial/example.yaml tutorial/files
-mv /tutorial/tutorial.yaml tutorial/files
-mv /tutorial/challenge.py tutorial/files
+mkdir -p /tutorial/files
+mv /tutorial/example.net /tutorial/files
+mv /tutorial/tutorial.net /tutorial/files
+mv /tutorial/loihi.yaml /tutorial/files
+mv /tutorial/example.yaml /tutorial/files
+mv /tutorial/tutorial.yaml /tutorial/files
+mv /tutorial/challenge.py /tutorial/files
