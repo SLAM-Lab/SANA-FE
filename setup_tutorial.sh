7655d1634f01dce838cd532d4cbbbc708ed6b95f
James Boyle <james.boyle@utexas.edu>
2024-04-08 19:40:34 -0500

    Try again with setup script

--- setup_tutorial.sh
+++ setup_tutorial.sh
@@ -1,4 +1,7 @@
 mkdir /tutorial/files
-mv /tutorial/*.net tutorial/files
-mv /tutorial/*.yaml tutorial/files
+mv /tutorial/example.net tutorial/files
+mv /tutorial/tutorial.net tutorial/files
+mv /tutorial/loihi.yaml tutorial/files
+mv /tutorial/example.yaml tutorial/files
+mv /tutorial/tutorial.yaml tutorial/files
 mv /tutorial/challenge.py tutorial/files
