bd9ce92f14d46271c6f2b696585de59819788dc4
James Boyle <james.boyle@utexas.edu>
2024-04-10 16:53:55 -0500

    tutorial: Change structure around, create new dir
    Add tutorial directory and make sure scripts can be run either
    in the repo or in the docker container.

--- setup_tutorial.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /tutorial/files
-mv /tutorial/example.net /tutorial/files
-mv /tutorial/tutorial.net /tutorial/files
-mv /tutorial/loihi.yaml /tutorial/files
-mv /tutorial/example.yaml /tutorial/files
-mv /tutorial/tutorial.yaml /tutorial/files
-mv /tutorial/challenge.py /tutorial/files
---
+++ tutorial/setup_tutorial.sh
@@ -0,0 +1,7 @@
+mkdir -p /tutorial/
+mv /home/example.net /tutorial/
+mv /home/tutorial.net /tutorial/
+mv /home/loihi.yaml /tutorial/
+mv /home/example.yaml /tutorial/
+mv /home/tutorial.yaml /tutorial/
+mv /home/challenge.py /tutorial/
