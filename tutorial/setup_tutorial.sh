c01a7e28a74102c4e1096291f52c1e09c68144b4
James Boyle <james.boyle@utexas.edu>
2024-04-10 17:15:25 -0500

    tutorial: More minor tutorial fixes..

--- tutorial/setup_tutorial.sh
+++ tutorial/setup_tutorial.sh
@@ -1,6 +1,7 @@
 set +e
 mkdir -p /tutorial/
--mv /home/snn.net /tutorial/
--mv /home/loihi.yaml /tutorial/
--mv /home/arch.yaml /tutorial/
--mv /home/challenge.py /tutorial/
+mv /home/snn.net /tutorial/
+mv /home/loihi.yaml /tutorial/
+mv /home/arch.yaml /tutorial/
+mv /home/challenge.py /tutorial/
+mv /home/dvs_challenge.npz /tutorial/
