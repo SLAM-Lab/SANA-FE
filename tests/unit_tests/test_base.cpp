315d761360d8f15f1ab5a6c42e1e23c403e125c9
arsen iman <arseniman976@gmail.com>
2025-07-16 17:39:06 -0500

    fixed a small issue in CMakeLists

--- tests/unit_tests/test_base.cpp
+++ tests/unit_tests/test_base.cpp
@@ -7,8 +7,7 @@ TEST(InitialTest, CheckTestFunctionality) {
 
 /*
     FOR WRITING NEW TESTS WITH GTEST PLEASE:
-    1. follow the naming convention test_<test name>.cpp to avoid linking issues
-    2. put any helper functions in the anonymous namespace at the top of the file
-    3. any files needed for the test can be put in the tests/data directory (to reference them in your tests use the relative path from the subdirectory of the test in the build directory,
+    1. put any helper functions in the anonymous namespace at the top of the file
+    2. any files needed for the test can be put in the tests/data directory (to reference them in your tests use the relative path from the subdirectory of the test in the build directory,
        e.g. "../../../tests/data/arch/test1.yaml")
 */
