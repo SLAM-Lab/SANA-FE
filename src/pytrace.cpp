9e880987189a9462c9f3fccc8229b5e7b283d4ab
James Boyle <james.boyle@utexas.edu>
2025-09-16 13:49:29 -0500

    Fix bug where Python trace timestep count resets
    The Python (file) traces reset the timestep count for spike and
    potential traces every time we run sim() from Python, which will
    cause issues in Python scripts that need to differentiate spikes
    and potentials recorded for each simulation run. Use the global
    timestep count, which increments every time we sim().
    
    While I'm at it, I realized the Python tracing code is misleading
    and confusing. Break the tracing into its own smaller file and
    provide different interfaces for the hardware traces and network
    traces (both are used differently in my pybind11 code). The
    hardware trace info is available in the Timestep struct, whereas
    the spikes and potentials are directly read from the SpikingChip
    struct (partly to save memory). Now the usage of both types of
    traces should be clearer and easier to understand next time I
    look.

---
+++ src/pytrace.cpp
@@ -0,0 +1,200 @@
+
+#include "pytrace.hpp"
+
+pybind11::dict message_to_dict(const sanafe::Message &m)
+{
+    pybind11::dict message_dict;
+
+    message_dict["generation_delay"] = m.generation_delay;
+    message_dict["network_delay"] = m.network_delay;
+    message_dict["receive_delay"] = m.receive_delay;
+    message_dict["blocked_delay"] = m.blocked_delay;
+    message_dict["sent_timestamp"] = m.sent_timestamp;
+    message_dict["received_timestamp"] = m.received_timestamp;
+    message_dict["processed_timestamp"] = m.processed_timestamp;
+    message_dict["timestep"] = m.timestep;
+    message_dict["mid"] = m.mid;
+    message_dict["spikes"] = m.spikes;
+    message_dict["hops"] = m.hops;
+    message_dict["src_neuron_offset"] = m.src_neuron_offset;
+    message_dict["src_neuron_group_id"] = m.src_neuron_group_id;
+    message_dict["src_x"] = m.src_x;
+    message_dict["dest_x"] = m.dest_x;
+    message_dict["src_y"] = m.src_y;
+    message_dict["dest_y"] = m.dest_y;
+    message_dict["src_tile_id"] = m.src_tile_id;
+    message_dict["src_core_id"] = m.src_core_id;
+    message_dict["src_core_offset"] = m.src_core_offset;
+    message_dict["dest_tile_id"] = m.dest_tile_id;
+    message_dict["dest_core_id"] = m.dest_core_id;
+    message_dict["dest_core_offset"] = m.dest_core_offset;
+    message_dict["dest_axon_hw"] = m.dest_axon_hw;
+    message_dict["dest_axon_id"] = m.dest_axon_id;
+    message_dict["placeholder"] = m.placeholder;
+
+    return message_dict;
+}
+
+std::map<std::string, PerfStatistic> timestep_data_to_map(
+        const sanafe::Timestep &ts)
+{
+    std::map<std::string, PerfStatistic> ts_map;
+
+    ts_map["timestep"] = ts.timestep;
+    ts_map["fired"] = ts.neurons_fired;
+    ts_map["updated"] = ts.neurons_updated;
+    ts_map["hops"] = ts.total_hops;
+    ts_map["spikes"] = ts.spike_count;
+    ts_map["sim_time"] = ts.sim_time;
+    ts_map["total_energy"] = ts.total_energy;
+
+    return ts_map;
+}
+
+void PyTrace::open(pybind11::object trace_obj, const bool overwrite_trace)
+{
+    mode = TraceMode::trace_none;
+    // The trace object can be None, a boolean (True/False) enabling memory
+    //  based traces, a string specifying a trace filename or a Python file
+    //  object
+    obj = trace_obj;
+
+    if (!trace_obj.is_none())
+    {
+        // Check if it's a boolean (True)
+        if (pybind11::isinstance<pybind11::bool_>(trace_obj))
+        {
+            const bool trace_enabled = trace_obj.cast<bool>();
+            if (trace_enabled)
+            {
+                mode = TraceMode::trace_memory;
+            }
+        }
+        // Check if it's a file-like object (i.e., has a 'write' method)
+        else if (pybind11::hasattr(trace_obj, "write"))
+        {
+            mode = TraceMode::trace_file;
+            // File handle will be used directly via Python calls
+
+            if (overwrite_trace)
+            {
+                obj.attr("seek")(0);
+            }
+        }
+        // Check if it's a string (filename)
+        else if (pybind11::isinstance<pybind11::str>(trace_obj))
+        {
+            mode = TraceMode::trace_file;
+            const auto filename = trace_obj.cast<std::string>();
+
+            const std::ios::openmode flags = overwrite_trace ?
+                    (std::ios::out | std::ios::trunc) :
+                    (std::ios::out | std::ios::app);
+            file.open(filename, flags);
+            if (!file.is_open())
+            {
+                throw std::runtime_error(
+                        "Failed to open trace file: " + filename);
+            }
+        }
+        else
+        {
+            throw std::invalid_argument(
+                    "trace_obj must be None, True, a filename string, "
+                    "or a file-like object");
+        }
+    }
+}
+
+void PyTrace::write_header()
+{
+    if (mode == TraceMode::trace_file)
+    {
+        std::ostringstream trace_ss;
+        get_header_string(trace_ss);
+        if (file.is_open())
+        {
+            // Write to C++ file stream
+            file << trace_ss.str();
+            file.flush(); // Ensure data is written
+        }
+        else if (!obj.is_none())
+        {
+            // Write to Python file handle
+            const pybind11::gil_scoped_acquire acquire_for_write;
+            obj.attr("write")(trace_ss.str());
+            if (pybind11::hasattr(obj, "flush"))
+            {
+                obj.attr("flush")();
+            }
+        }
+    }
+}
+
+void PyHardwareTrace::record_timestep(const sanafe::Timestep &ts)
+{
+    if (mode == TraceMode::trace_memory)
+    {
+        // Note we must always acquire the GIL before modifying Python objects
+        const pybind11::gil_scoped_acquire acquire;
+        // Make call to appropriate overriden virtual routine
+        append_trace_data(ts);
+    }
+    else if (mode == TraceMode::trace_file)
+    {
+        std::ostringstream trace_ss;
+        // Make call to appropriate overriden virtual routine
+        get_trace_string(trace_ss, ts);
+        if (file.is_open())
+        {
+            // Write to C++ file stream
+            file << trace_ss.str();
+            file.flush(); // Ensure data is written
+        }
+        else if (!obj.is_none())
+        {
+            // Write to Python file handle
+            const pybind11::gil_scoped_acquire acquire_for_write;
+            obj.attr("write")(trace_ss.str());
+            if (pybind11::hasattr(obj, "flush"))
+            {
+                obj.attr("flush")();
+            }
+        }
+    }
+    // else do nothing
+}
+
+void PyNetworkTrace::record_chip_data(const long int timestep)
+{
+    if (mode == TraceMode::trace_memory)
+    {
+        // Note we must always acquire the GIL before modifying Python objects
+        const pybind11::gil_scoped_acquire acquire;
+        // Make call to appropriate overriden virtual routine
+        append_trace_data();
+    }
+    else if (mode == TraceMode::trace_file)
+    {
+        std::ostringstream trace_ss;
+        // Make call to appropriate overriden virtual routine
+        get_trace_string(trace_ss, timestep);
+        if (file.is_open())
+        {
+            // Write to C++ file stream
+            file << trace_ss.str();
+            file.flush(); // Ensure data is written
+        }
+        else if (!obj.is_none())
+        {
+            // Write to Python file handle
+            const pybind11::gil_scoped_acquire acquire_for_write;
+            obj.attr("write")(trace_ss.str());
+            if (pybind11::hasattr(obj, "flush"))
+            {
+                obj.attr("flush")();
+            }
+        }
+    }
+    // else do nothing
+}
---
+++ src/pytrace.hpp
@@ -0,0 +1,316 @@
+// Copyright (c) 2025 - The University of Texas at Austin
+//  This work was produced under contract #2317831 to National Technology and
+//  Engineering Solutions of Sandia, LLC which is under contract
+//  No. DE-NA0003525 with the U.S. Department of Energy.
+// pytrace.hpp
+#ifndef PYTRACE_HEADER_INCLUDED_
+#define PYTRACE_HEADER_INCLUDED_
+
+#include <map>
+#include <variant>
+
+#include <pybind11/pybind11.h>
+
+#include "fwd.hpp"
+#include "message.hpp"
+#include "network.hpp"
+#include "timestep.hpp"
+#include "schedule.hpp"
+
+// Forward declarations
+using PerfStatistic = std::variant<size_t, long int, double>;
+
+pybind11::dict message_to_dict(const sanafe::Message &m);
+std::map<std::string, PerfStatistic> timestep_data_to_map(
+        const sanafe::Timestep &ts);
+
+// Determine spike trace mode
+enum TraceMode : uint8_t
+{
+    trace_none = 0UL, // No tracing
+    trace_file = 1UL, // Write to file
+    trace_memory = 2UL, // Store in memory for return
+};
+
+class PyTrace
+{
+public:
+    PyTrace(sanafe::SpikingChip *chip, pybind11::object &trace_obj,
+            const bool overwrite_trace)
+            : parent_chip(chip)
+    {
+        open(trace_obj, overwrite_trace);
+    }
+    virtual ~PyTrace()
+    {
+        if (file.is_open())
+        {
+            file.close();
+        }
+    }
+    PyTrace(const PyTrace &&move) = delete;
+    PyTrace &operator=(const PyTrace &copy) = delete;
+    PyTrace &operator=(PyTrace &&move) = delete;
+
+    void write_header();
+    void open(pybind11::object trace_obj, const bool overwrite_trace);
+
+    virtual pybind11::object get_python_object() const = 0;
+
+protected:
+    sanafe::SpikingChip *parent_chip;
+    pybind11::object obj{pybind11::none()};
+    TraceMode mode{TraceMode::trace_none};
+    std::ofstream file;
+
+private:
+    virtual void get_header_string(std::ostringstream &ss) = 0;
+};
+
+class PyHardwareTrace : public PyTrace
+{
+public:
+    PyHardwareTrace(sanafe::SpikingChip *chip, pybind11::object &trace_obj,
+            const bool overwrite_trace) : PyTrace(chip, trace_obj, overwrite_trace) {}
+    void record_timestep(const sanafe::Timestep &ts);
+
+private:
+    virtual void get_trace_string(
+            std::ostringstream &ss, const sanafe::Timestep &ts) = 0;
+    virtual void append_trace_data(const sanafe::Timestep &ts) = 0;
+};
+
+class PyNetworkTrace : public PyTrace
+{
+public:
+    PyNetworkTrace(sanafe::SpikingChip *chip, pybind11::object &trace_obj,
+            const bool overwrite_trace)
+            : PyTrace(chip, trace_obj, overwrite_trace) {}
+    void record_chip_data(long int timestep);
+
+private:
+    virtual void get_trace_string(std::ostringstream &ss, long int timestep) = 0;
+    virtual void append_trace_data() = 0;
+};
+
+class PySpikeTrace : public PyNetworkTrace
+{
+public:
+    PySpikeTrace(sanafe::SpikingChip *chip, pybind11::object trace_obj,
+            const bool overwrite_trace)
+            : PyNetworkTrace(chip, trace_obj, overwrite_trace)
+    {
+    }
+    ~PySpikeTrace() override = default;
+    void get_header_string(std::ostringstream &ss) override
+    {
+        sanafe::SpikingChip::sim_trace_write_spike_header(ss);
+    }
+    void get_trace_string(
+            std::ostringstream &ss, const long int timestep) override
+    {
+        parent_chip->sim_trace_record_spikes(ss, timestep);
+    }
+    void append_trace_data() override
+    {
+        auto spikes = parent_chip->get_spikes();
+        data.emplace_back(std::move(spikes));
+    }
+    pybind11::object get_python_object() const override
+    {
+        // Acquire GIL to be safe, even though simulations should have finished
+        //  at this point and the GIL should be reacquired already
+        const pybind11::gil_scoped_acquire acquire;
+        if (mode == TraceMode::trace_memory)
+        {
+            pybind11::list data_list;
+            for (const auto &spikes : data)
+            {
+                pybind11::list spike_list;
+                for (const sanafe::NeuronAddress &spike : spikes)
+                {
+                    spike_list.append(pybind11::cast(spike));
+                }
+                data_list.append(spike_list);
+            }
+            return data_list;
+        }
+        return pybind11::none();
+    }
+
+private:
+    std::vector<std::vector<sanafe::NeuronAddress>> data;
+};
+
+class PyPotentialTrace : public PyNetworkTrace
+{
+public:
+    PyPotentialTrace(sanafe::SpikingChip *chip, pybind11::object trace_obj,
+            const bool overwrite_trace)
+            : PyNetworkTrace(chip, trace_obj, overwrite_trace)
+    {
+    }
+    ~PyPotentialTrace() override = default;
+    void get_header_string(std::ostringstream &ss) override
+    {
+        parent_chip->sim_trace_write_potential_header(ss);
+    }
+    void get_trace_string(
+            std::ostringstream &ss, const long int timestep) override
+    {
+        parent_chip->sim_trace_record_potentials(ss, timestep);
+    }
+    void append_trace_data() override
+    {
+        auto potentials = parent_chip->get_potentials();
+        data.emplace_back(std::move(potentials));
+    }
+    pybind11::object get_python_object() const override
+    {
+        // Acquire GIL to be safe, even though simulations should have finished
+        //  at this point and the GIL should be reacquired already
+        const pybind11::gil_scoped_acquire acquire;
+        if (mode == TraceMode::trace_memory)
+        {
+            return pybind11::cast(data);
+        }
+        return pybind11::none();
+    }
+
+private:
+    std::vector<std::vector<double>> data;
+};
+
+class PyPerfTrace : public PyHardwareTrace
+{
+public:
+    ~PyPerfTrace() override = default;
+    PyPerfTrace(sanafe::SpikingChip *chip, pybind11::object trace_obj,
+            const bool overwrite_trace)
+            : PyHardwareTrace(chip, trace_obj, overwrite_trace)
+    {
+    }
+    void get_header_string(std::ostringstream &ss) override
+    {
+        parent_chip->sim_trace_write_perf_header(ss);
+    }
+    void get_trace_string(
+            std::ostringstream &ss, const sanafe::Timestep &ts) override
+    {
+        parent_chip->sim_trace_record_perf(ss, ts);
+    }
+    void append_trace_data(const sanafe::Timestep &ts) override
+    {
+        std::map<std::string, PerfStatistic> stats = timestep_data_to_map(ts);
+
+        // Get any optional traces and cast double values to Python objects
+        const std::map<std::string, double> optional_perf_traces =
+                parent_chip->sim_trace_get_optional_traces();
+        for (const auto &[name, value] : optional_perf_traces)
+        {
+            stats[name] = value;
+        }
+
+        for (const auto &[key, value] : stats)
+        {
+            data[key].emplace_back(value);
+        }
+    }
+    pybind11::object get_python_object() const override
+    {
+        // Acquire GIL to be safe, even though simulations should have finished
+        //  at this point and the GIL should be reacquired already
+        const pybind11::gil_scoped_acquire acquire;
+        if (mode == TraceMode::trace_memory)
+        {
+            return pybind11::cast(data);
+        }
+        return pybind11::none();
+    }
+
+private:
+    std::map<std::string, std::vector<PerfStatistic>> data;
+};
+
+class PyMessageTrace : public PyHardwareTrace
+{
+public:
+    PyMessageTrace(sanafe::SpikingChip *chip, pybind11::object trace_obj,
+            const bool overwrite_trace)
+            : PyHardwareTrace(chip, trace_obj, overwrite_trace)
+    {
+    }
+    ~PyMessageTrace() override = default;
+    void get_header_string(std::ostringstream &ss) override
+    {
+        sanafe::SpikingChip::sim_trace_write_message_header(ss);
+    }
+    void get_trace_string(
+            std::ostringstream &ss, const sanafe::Timestep &ts) override
+    {
+        std::vector<std::reference_wrapper<const sanafe::Message>> all_messages;
+        for (const sanafe::MessageFifo &q : ts.messages)
+        {
+            for (const sanafe::Message &m : q)
+            {
+                all_messages.emplace_back(m);
+            }
+        }
+        // Sort messages in message ID order (with placeholders last)
+        std::sort(all_messages.begin(), all_messages.end(),
+                sanafe::CompareMessagesByID{});
+        // Save the messages in sorted order
+        for (const sanafe::Message &m : all_messages)
+        {
+            sim_trace_record_message(ss, m);
+        }
+    }
+    void append_trace_data(const sanafe::Timestep &ts) override
+    {
+        std::vector<sanafe::Message> timestep_messages;
+
+        // Not crucial, but its nice to print messages in ID order.
+        //  Copy all the messages into a single vector and sort
+        for (const sanafe::MessageFifo &q : ts.messages)
+        {
+            for (const sanafe::Message &m : q)
+            {
+                timestep_messages.emplace_back(m);
+            }
+        }
+        // Sort messages in message order, starting at lowest mid first
+        std::sort(timestep_messages.begin(), timestep_messages.end(),
+                [](const sanafe::Message &left, const sanafe::Message &right) {
+                    return left.mid < right.mid;
+                });
+        data.emplace_back(std::move(timestep_messages));
+    }
+    pybind11::object get_python_object() const override
+    {
+        // Acquire GIL to be safe, even though simulations should have finished
+        //  at this point and the GIL should be reacquired already
+        const pybind11::gil_scoped_acquire acquire;
+        if (mode == TraceMode::trace_memory)
+        {
+            // Convert each message to a dict, meaning we need to manually
+            //  iterate over every message in data
+            pybind11::list data_list;
+            for (const auto &ts_messages : data)
+            {
+                pybind11::list ts_list;
+                for (const auto &m : ts_messages)
+                {
+                    ts_list.append(message_to_dict(m));
+                }
+                data_list.append(ts_list);
+            }
+            return data_list;
+        }
+        return pybind11::none();
+    }
+
+private:
+    std::vector<std::vector<sanafe::Message>> data;
+};
+
+#endif
