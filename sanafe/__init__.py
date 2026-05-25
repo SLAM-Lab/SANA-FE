320e2091c51119ff9396981232caa3091a9ebd3b
James Boyle <james.boyle@utexas.edu>
2026-05-25 15:17:56 -0500

    Update tutorials and package YAML descriptions
    Remove the flaky dependency on downloaded files from github or Google
    drive and package essential tutorial files within the sanafe package.
    Now we can simply reference them within the Python library.
    
    This includes the DVS gesture weights, which are relatively small and
    now don't have to be downloaded from a gdrive location.

--- sanafe/__init__.py
+++ sanafe/__init__.py
@@ -1,3 +1,3 @@
 # Import pybind11 (C++) kernel under top-level
 from sanafecpp import *
-from ._examples import load_example
+from ._examples import load_example, load_loihi, load_truenorth
--- sanafe/_examples.py
+++ sanafe/_examples.py
@@ -7,3 +7,15 @@ def load_example():
     arch = load_arch(base / "example_chip.yaml")
     net  = load_net(base / "example_snn.yaml", arch)
     return arch, net
+
+def load_loihi():
+    """Loihi the Loihi architecture file"""
+    base = files("sanafe.examples")
+    loihi_arch = load_arch(base / "loihi.yaml")
+    return loihi_arch
+
+def load_truenorth():
+    """Loihi the Loihi architecture file"""
+    base = files("sanafe.examples")
+    truenorth_arch = load_arch(base / "truenorth.yaml")
+    return truenorth_arch
